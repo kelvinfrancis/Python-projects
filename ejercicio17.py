860ea987e89a603fb4f6df0e83359411ecf23528
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-15 16:58:00 -0400

    commit

---
+++ ejercicio17.py
@@ -0,0 +1,4 @@
+#Confeccionar un programa que permita cargar un número entero positivo de hasta tres cifras y muestre un mensaje indicando si tiene 1, 2, o 3 cifras. 
+#Mostrar un mensaje de error si el número de cifras es mayor.
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=7&codigo=7&inicio=0
+
