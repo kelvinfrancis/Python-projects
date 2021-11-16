86266684aaff3ab06e5ab42de69b5076437abd8f
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-16 11:06:35 -0400

    commit

--- ejercicio17.py
+++ ejercicio17.py
@@ -2,3 +2,17 @@
 #Mostrar un mensaje de error si el número de cifras es mayor.
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=7&codigo=7&inicio=0
 
+num = int(input("Ingrese un numero menor de 3 cifras:"))
+
+if num<10:
+    print("Este numero tiene UN digito.")
+
+elif num<100:
+    print("Este numero tiene DOS digitos.")
+
+elif num<1000:
+    print("Este numero tiene TRES digitos.")
+
+else:
+    print("Este numero tiene MAS de TRES digitos.")
+
