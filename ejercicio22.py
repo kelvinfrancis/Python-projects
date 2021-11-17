a8e79275d9f993c4ba859327bba7fafce2bc96ea
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-17 11:09:44 -0400

    commit

---
+++ ejercicio22.py
@@ -0,0 +1,7 @@
+#Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10, 
+#imprimir en pantalla la leyenda "Todos los números son menores a diez".
+
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
+
