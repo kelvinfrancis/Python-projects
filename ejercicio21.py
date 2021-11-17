eb6f3b8a5db66ac09a832c48ddda0c1e3fc69338
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-17 10:58:03 -0400

    commit

---
+++ ejercicio21.py
@@ -0,0 +1,9 @@
+#Realizar un programa que pida cargar una fecha cualquiera, 
+#luego verificar si dicha fecha corresponde a Navidad.
+
+dia = int(input("Ingrese el dia: "))
+mes = int(input("Ingrese el mes: "))
+year = int(input("Ingrese el año: "))
+
+if mes == 12 and dia == 25:
+    print("Es navidad.!")
