118d63ce340955d714fce2dcb1423d0e8b60d14a
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-18 10:58:21 -0400

    commit

--- ejercicio25.py
+++ ejercicio25.py
@@ -3,3 +3,20 @@
 #Posteriormente imprimir en pantalla en que cuadrante se ubica dicho punto. 
 #(1º Cuadrante si x > 0 Y y > 0 , 2º Cuadrante: x < 0 Y y > 0, etc.)
 
+x = int(input("Ingrese el valor de X: "))
+y = int(input("Ingrese el valor de Y: "))
+
+if x == 0 and y == 0:
+    print("Ingrese valores diferentes de cero. ")
+elif x>0 and y>0:
+    print("1er cuadrante.")
+
+elif x<0 and y>0:
+    print("2do cuadrante.")
+
+elif x<0 and y<0:
+    print("3er cuadrante.")
+else:
+    print("4to cuadrante")
+
+
