ac6340b9af62427f941d0df17b0198c55cfd3992
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-19 12:32:33 -0400

    commit

--- ejercicio26.py
+++ ejercicio26.py
@@ -1,8 +1,13 @@
 # De un operario se conoce su sueldo y los años de antigüedad. 
 #Se pide confeccionar un programa que lea los datos de entrada e informe:
-# a) Si el sueldo es inferior a 500 y su antigüedad es igual o superior a 10 años, otorgarle un aumento del 20 %, 
-# mostrar el sueldo a pagar.
-# b)Si el sueldo es inferior a 500 pero su antigüedad es menor a 10 años, otorgarle un aumento de 5 %.
+# a) Si el sueldo es inferior a 500 y su antigüedad es igual o superior a 10 años, 
+# otorgarle un aumento del 20 %, mostrar el sueldo a pagar.
+# b)Si el sueldo es inferior a 500 pero su antigüedad es menor a 10 años, 
+# otorgarle un aumento de 5 %.
 # c) Si el sueldo es mayor o igual a 500 mostrar el sueldo en pantalla sin cambios
 
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=8&codigo=8&inicio=0
+
+sueldo = int(input("Ingrese su sueldo: "))
+tiempo = int(input("Ingrese los años trabajados: "))
+
