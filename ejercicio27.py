cf11d544fcfa7351fda2f0b9de923830f168f04e
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-19 17:18:59 -0400

    commit

--- ejercicio27.py
+++ ejercicio27.py
@@ -1,3 +1,4 @@
 #Escribir un programa en el cual: dada una lista de tres valores numéricos distintos 
 # se calcule e informe su rango de variación (debe mostrar el mayor y el menor de ellos)
 
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=8&codigo=8&inicio=0
