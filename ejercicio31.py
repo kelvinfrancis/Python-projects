665ad68a7536460950e9b821d7ea4566cf732127
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-25 07:58:23 -0400

    COMMIT

---
+++ ejercicio31.py
@@ -0,0 +1,15 @@
+#--PROBLEMA 4--- https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=9&codigo=9&inicio=0
+#Una planta que fabrica perfiles de hierro posee un lote de n piezas.
+#Confeccionar un programa que pida ingresar por teclado la cantidad de piezas a procesar y luego ingrese la longitud de cada perfil;
+#sabiendo que la pieza cuya longitud esté comprendida en el rango de 1.20 y 1.30 son aptas.
+# Imprimir por pantalla la cantidad de piezas aptas que hay en el lote.
+
+x=1
+n=int(input("Ingrese la cantidad de piezas que desea ingresar: "))
+
+while x<=n:
+    longitudPieza = float(input("Ingrese la longitud de la pieza: "))
+    x=x+1
+if 1.20<longitudPieza<1.30:
+    print("Las piezas con las longitudes correctas son:")
+    print(longitudPieza)
