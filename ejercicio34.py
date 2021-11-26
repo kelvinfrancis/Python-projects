8460a51c35bb6ae6ce177a30115becbcd62b8af2
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-26 12:09:11 -0400

    Realizado

--- ejercicio34.py
+++ ejercicio34.py
@@ -5,13 +5,14 @@
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=9&codigo=9&inicio=0
 
 n=int(input("Ingrese la cantidad de empleados: "))
-x=1
 entre100y300 = 0
 masde300 = 0
+x=1
 importe=0
+
 while x<=n:
-    sueldo=int(input(print("Ingrese el sueldo del empleado:")))
-    if 100<sueldo<=300:
+    sueldo=int(input("Ingrese el sueldo del empleado:"))
+    if 100<=sueldo<=300:
         entre100y300=entre100y300+1
     elif sueldo>300:
         masde300=masde300+1
@@ -23,3 +24,4 @@ print("La cantidad de empleados que ganan mas de $300 es: ")
 print(masde300)
 print("El importe que gasta la empresa en sueldo al personal es: ")
 print(importe)
+
