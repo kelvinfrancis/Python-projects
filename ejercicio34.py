f7f8d7c202ca2063c2a2fa7a0e8d9e87399b97ba
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-11-25 23:24:48 -0400

    commit

---
+++ ejercicio34.py
@@ -0,0 +1,5 @@
+#En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y $500, 
+#realizar un programa que lea los sueldos que cobra cada empleado e informe cuántos empleados cobran entre $100 y $300 
+#y cuántos cobran más de $300. Además el programa deberá informar el importe que gasta la empresa en sueldos al personal.
+
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=9&codigo=9&inicio=0
