d87057b5216132bbb3321eca111294bada76e4fa
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-03 11:30:47 -0400

    Te cuenta 25 terminos de 11 en 11 sin ingresar datos

---
+++ ejercicio35.py
@@ -0,0 +1,8 @@
+# Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. (No se ingresan valores por teclado)
+n=1
+x=11
+print("--25 terminos de la serie de 11 en 11--")
+while n<=25:
+    print(x)
+    x=x+11
+    n=n+1
