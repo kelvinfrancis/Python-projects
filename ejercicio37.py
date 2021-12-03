af5ecf2428c0325197b88b78ac7b3dbbeb5fa47f
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-03 11:46:15 -0400

    commit

---
+++ ejercicio37.py
@@ -0,0 +1,4 @@
+#Realizar un programa que permita cargar dos listas de 15 valores cada una. 
+#Informar con un mensaje cual de las dos listas tiene un valor acumulado mayor 
+#(mensajes "Lista 1 mayor", "Lista 2 mayor", "Listas iguales")
+#Tener en cuenta que puede haber dos o más estructuras repetitivas en un algoritmo.
