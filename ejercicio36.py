efef45639b556b0a00f4dae72773dbf85dccc2e8
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-03 11:40:20 -0400

    Presenta los multiplos de 8 hata el numero 500

---
+++ ejercicio36.py
@@ -0,0 +1,8 @@
+# Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16 - 24, etc.
+n=8
+x=1
+print("----Multiplos de 8 hasta el 500----")
+while n<500:
+    print(n)
+    n=n+8
+    x=x+1
