0c7bacd56eef18e8dd810cd2b6d028ca361b4bd8
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-07 16:21:05 -0400

    ready

--- ejercicio43.py
+++ ejercicio43.py
@@ -2,9 +2,9 @@
 # Este problema ya lo desarrollamos, lo resolveremos empleando la estructura for para repetir la carga de los diez valores por teclado.
 sum=0
 promedio=0
-for x in range(11):
+for x in range(10):
     valor = int(input("Ingrese un valor: "))
     sum = valor+sum
-
 promedio = sum/10
 print("La suma total de los valores es: ", sum)
+print("El promedio total de los valores es: ", promedio)
