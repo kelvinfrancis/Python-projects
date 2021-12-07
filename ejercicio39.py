388d141d462e4dcbb977bf682f50bbfc1086a0b6
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-07 10:14:20 -0400

    commit

--- ejercicio39.py
+++ ejercicio39.py
@@ -1,3 +1,4 @@
 #Realizar un programa que imprima en pantalla los números del 0 al 100. 
 #Este problema lo podemos resolver perfectamente con el ciclo while pero en esta situación lo resolveremos empleando el for.
 
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=10&codigo=10&inicio=0
