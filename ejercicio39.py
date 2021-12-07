3e855839c0957f5688c543f5356ede1278a4631a
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-07 15:51:43 -0400

    Imprimir los numeros del 0-100 usando FOR

--- ejercicio39.py
+++ ejercicio39.py
@@ -2,3 +2,6 @@
 #Este problema lo podemos resolver perfectamente con el ciclo while pero en esta situación lo resolveremos empleando el for.
 
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=10&codigo=10&inicio=0
+
+for x in range 101:
+    print(x)
