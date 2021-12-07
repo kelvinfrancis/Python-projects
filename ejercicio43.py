fc45f6891ce37bf5a11d96b33b01cc92ec534db8
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-07 16:20:34 -0400

    Ingresa 10 valores con FOR, imprime la suma y el promedio de estos

---
+++ ejercicio43.py
@@ -0,0 +1,10 @@
+# Desarrollar un programa que permita la carga de 10 valores por teclado y nos muestre posteriormente la suma de los valores ingresados y su promedio. 
+# Este problema ya lo desarrollamos, lo resolveremos empleando la estructura for para repetir la carga de los diez valores por teclado.
+sum=0
+promedio=0
+for x in range(11):
+    valor = int(input("Ingrese un valor: "))
+    sum = valor+sum
+
+promedio = sum/10
+print("La suma total de los valores es: ", sum)
