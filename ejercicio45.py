510ab54bfd050aa8b6be942b830eae3d3e89e6e5
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-14 10:06:29 -0400

    commit

---
+++ ejercicio45.py
@@ -0,0 +1,2 @@
+#Escribir un programa que lea 10 números enteros y luego muestre cuántos valores ingresados fueron múltiplos de 3 y cuántos de 5. 
+#Debemos tener en cuenta que hay números que son múltiplos de 3 y de 5 a la vez.
