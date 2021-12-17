7538702425aa5b999acfc6a90e549be570e1c6a6
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-17 12:53:49 -0400

    commit

---
+++ ejercicio47.py
@@ -0,0 +1,4 @@
+# Confeccionar un programa que lea n pares de datos, cada par de datos corresponde a la medida de la base 
+# y la altura de un triángulo. El programa deberá informar:
+# a) De cada triángulo la medida de su base, su altura y su superficie.
+# b) La cantidad de triángulos cuya superficie es mayor a 12.
