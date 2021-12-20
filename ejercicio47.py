3f542da2fa692088840b987f5ffe46b70b438d05
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-20 09:36:25 -0400

    Commit

--- ejercicio47.py
+++ ejercicio47.py
@@ -5,6 +5,15 @@
 
 n = int(input("Ingrese la cantidad de triangulos: "))
 x=0
-
+c=1
+superficiemayor=0
 for x in range(n):
+    base = float(input("Ingrese la base del triangulo: "))
+    altura = float(input("Ingrese la altura del triangulo: "))
+    superficie = base*altura
+    print("Triangulo ",c,"Base: ",base," Altura: ",altura," Superficie: ",superficie)
+    x=x+1
+    if superficie >12:
+        superficiemayor=superficiemayor+1
     
+print("La cantidad de triangulos cuya superficie es mayor a 12 es: ",superficiemayor)
