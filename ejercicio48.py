3ccfed59e1ec6f948906b25c1ab8ea36eb341a41
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2021-12-30 16:33:27 -0400

    Ingresa 10 valores e imprime la suma de los ultimos 5 valores ingresados

--- ejercicio48.py
+++ ejercicio48.py
@@ -1,2 +1,11 @@
 # Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los últimos 5 valores ingresados
 
+n=0
+suma=0
+for n in range(10):
+    valor = int(input("Ingrese un valor: "))
+    n=n+1
+    if n>5:
+        suma=valor+suma
+        
+print("La suma de los ultimos 5 valores es: ", suma)
