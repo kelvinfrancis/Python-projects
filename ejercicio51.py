90bfad9885151672117f4a03d6d3e482d12740f7
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-11 17:04:40 -0400

    comit

--- ejercicio51.py
+++ ejercicio51.py
@@ -1,3 +1,4 @@
 #Realizar un programa que lea los lados de n triángulos, e informar:
 #a) De cada uno de ellos, qué tipo de triángulo es: equilátero (tres lados iguales), isósceles (dos lados iguales), o escaleno (ningún lado igual)
 #b) Cantidad de triángulos de cada tipo.
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=10&codigo=10&inicio=0
