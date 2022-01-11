47bd81983a4ca167ab27aeaa33d20bf6091dccd1
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-11 15:26:35 -0400

    Tabla multiplicacion del 5

--- ejercicio49.py
+++ ejercicio49.py
@@ -1,10 +1,8 @@
 #Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5 al 50)
-
-
 x=1
 n=0
+print("--- Tabla de multiplicacion del 5 ---")
 for n in range(10):
     multi = 5*x
-    print("5x",x," = ",multi)
+    print("5 x",x,"= ",multi)
     x=x+1
-    
