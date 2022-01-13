b9644776f904d69cfe0280685e0a397935b85705
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-13 16:38:04 -0400

    ejercicio guardado

---
+++ ejercicio53.py
@@ -0,0 +1,7 @@
+#Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
+#a) La cantidad de valores ingresados negativos.
+#b) La cantidad de valores ingresados positivos.
+#c) La cantidad de múltiplos de 15.
+#d) El valor acumulado de los números ingresados que son pares.
+
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=10&codigo=10&inicio=0
