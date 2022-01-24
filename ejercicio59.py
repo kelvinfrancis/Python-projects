f360f9642a7545b395f896a9e00318276708715a
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-24 14:04:35 -0400

    Realizado

--- ejercicio59.py
+++ ejercicio59.py
@@ -4,6 +4,6 @@ n1=input("Ingrese el primer nombre:")
 n2=input("Ingrese el segundo nombre:")
 
 if n1>n2:
-    print("El mayor de los nombres es:",n1)
+    print(n1,"es mayor alfabeticamente.")
 else:
-    print("El mayor de los nombres es:",n2)
+    print(n2,"es mayor alfabeticamente.")
