7b8875406719b4c1edc81c898830bca5445785e1
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-24 14:03:08 -0400

    Ingresa dos nombres y muestra el mayor de estos alfabeticamente

---
+++ ejercicio59.py
@@ -0,0 +1,9 @@
+'''Realizar la carga de dos nombres por teclado. Mostrar cual de los dos es mayor alfabéticamente o si son iguales.'''
+
+n1=input("Ingrese el primer nombre:")
+n2=input("Ingrese el segundo nombre:")
+
+if n1>n2:
+    print("El mayor de los nombres es:",n1)
+else:
+    print("El mayor de los nombres es:",n2)
