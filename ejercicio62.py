7b8cb0f9a0b86bf5d16d446e82dfe515bafd089d
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-24 15:09:30 -0400

    Ingresa un nombre y te imprime la primera letra y la cantidad de letras que tiene el nombre

---
+++ ejercicio62.py
@@ -0,0 +1,7 @@
+'''Realizar la carga del nombre de una persona y 
+luego mostrar el primer caracter del nombre y 
+la cantidad de letras que lo componen.'''
+
+n1=input("Ingrese un nombre:")
+print("La primera letra es",n1[0])
+print("La cantidad de letras que componen el nombre es:",len(n1))
