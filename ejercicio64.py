d97c07d334a5912811049dca0eb1f2657118e4a6
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-24 15:17:06 -0400

    commit

--- ejercicio64.py
+++ ejercicio64.py
@@ -1,2 +1,2 @@
 '''Ingresar un mail por teclado. Verificar si el string ingresado contiene solo un caracter "@"'''
-
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=13&codigo=13&inicio=0
