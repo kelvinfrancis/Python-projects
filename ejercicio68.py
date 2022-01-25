688f6e06aea2ad9955224e878f567dc91c1e0a39
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-25 15:50:09 -0400

    ingresa una clave entre 10 y 20 caracteres, te muestra  si el largo es correcto o no

--- ejercicio68.py
+++ ejercicio68.py
@@ -1,3 +1,8 @@
 '''Solicitar el ingreso de una clave por teclado y almacenarla en una cadena de caracteres. 
 Controlar que el string ingresado tenga entre 10 y 20 caracteres para que sea válido, en caso contrario mostrar un mensaje de error.'''
 
+clave = input("Ingrese una clave entre 10 y 20 caracteres:")
+if 10<=len(clave)<=20:
+    print("Clave valida")
+else:
+    print("ERROR, clave no valida")
