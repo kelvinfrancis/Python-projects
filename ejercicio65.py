17a74ef4fec82779939827004bbfce0e012188c7
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-25 12:34:27 -0400

    Ingresa Un nombre y te imprime el nombre en mayuscula minuscula y con la primera letra mayuscula

---
+++ ejercicio65.py
@@ -0,0 +1,11 @@
+'''Inicializar un string con la cadena "mAriA" luego llamar a sus métodos upper(), lower() y capitalize(), 
+guardar los datos retornados en otros string y mostrarlos por pantalla.'''
+
+nombre=input("Ingrese el nombre 'mAriA':")
+mayuscula= nombre.upper()
+minuscula= nombre.lower()
+capitalizado= nombre.capitalize()
+
+print("Todo en mayuscula:",mayuscula)
+print("Todo en minuscula:",minuscula)
+print("Primera letra en mayuscula:",capitalizado)
