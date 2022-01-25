9490fff58642e28a86fb07d2797e0e895267e15c
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-25 16:25:17 -0400

    commit

---
+++ ejercicio69.py
@@ -0,0 +1,2 @@
+'''Definir una lista que almacene 5 enteros. Sumar todos sus elementos y mostrar dicha suma.'''
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=14&codigo=14&inicio=0
