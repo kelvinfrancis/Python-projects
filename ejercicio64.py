82cb8db6f75833cf168838cc8dd5355d48712d0b
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-25 12:13:37 -0400

    identifica si el correo tiene dos @

--- ejercicio64.py
+++ ejercicio64.py
@@ -1,2 +1,8 @@
 '''Ingresar un mail por teclado. Verificar si el string ingresado contiene solo un caracter "@"'''
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=13&codigo=13&inicio=0
+
+email=input("Ingrese un correo electronico: ")
+ocurrencias=email.count('@')
+while ocurrencias>=2:
+    email=input("Correo electronico no valido, favor ingresar un correo valido: ")
+print("Correo electronico valido, solo tiene una @.")
