15fd7b0c8a3208fd7b3d8c02b5c7192b55d5d839
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-25 15:36:23 -0400

    commit

--- ejercicio68.py
+++ ejercicio68.py
@@ -1,2 +1,3 @@
 '''Solicitar el ingreso de una clave por teclado y almacenarla en una cadena de caracteres. 
 Controlar que el string ingresado tenga entre 10 y 20 caracteres para que sea válido, en caso contrario mostrar un mensaje de error.'''
+
