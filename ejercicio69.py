159ffb70f8334b7209e6a063459fe54efd42f459
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-26 12:37:33 -0400

    Suma los valores de la lista, e imprime el total

--- ejercicio69.py
+++ ejercicio69.py
@@ -1,2 +1,10 @@
 '''Definir una lista que almacene 5 enteros. Sumar todos sus elementos y mostrar dicha suma.'''
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=14&codigo=14&inicio=0
+
+lista=[1,2,3,4,5]
+suma=0
+x=0
+while x<len(lista):
+    suma=suma+lista[x]
+    x=x+1
+print("La suma total de los valores es:",suma)
