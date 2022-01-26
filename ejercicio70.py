4ce652c0f42f9f2a60e80cb93610c2ec047fa511
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-26 12:42:59 -0400

    Ingresa cuatro meses, imprime el primero y el ultimo

---
+++ ejercicio70.py
@@ -0,0 +1,6 @@
+'''Definir una lista por asignación que almacene los nombres de los primeros cuatro meses de año. Mostrar el primer y último elemento de la lista solamente.'''
+
+meses=["Enero","febrero","marzo","Abril"]
+print("Meses ingresados",meses)
+print("El primer mes ingresado es:",meses[0])
+print("El ultimo mes ingresado es:",meses[3])
