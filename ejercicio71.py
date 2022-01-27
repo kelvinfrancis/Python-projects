25c6bfba6f3d6a8726e6a3dbaf3e1097890f66a3
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-27 11:00:59 -0400

    ingresa un nombre y dos notas, imprime el promedio de las notas

--- ejercicio71.py
+++ ejercicio71.py
@@ -1,2 +1,8 @@
 '''Definir una lista por asignación que almacene en la primer componente el nombre de un alumno y en las dos siguientes sus notas. 
 Imprimir luego el nombre y el promedio de las dos notas.'''
+
+alumno=['Kelvin',98,88]
+promedio=(alumno[1]+alumno[2])/2
+print("Datos ingresados:",alumno)
+print("Nombre del alumno:",alumno[0])
+print("Promedio de notas:",promedio)
