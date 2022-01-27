936f4af9258d7a7f28d430348342bfafbae104bf
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-27 10:01:29 -0400

    commit

---
+++ ejercicio71.py
@@ -0,0 +1,2 @@
+'''Definir una lista por asignación que almacene en la primer componente el nombre de un alumno y en las dos siguientes sus notas. 
+Imprimir luego el nombre y el promedio de las dos notas.'''
