7a351f931b5ecb44dcfd8b23152f181be82d9df0
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-28 10:45:36 -0400

    Crea una lista de 5 num. enteros ingresados por teclado. al final imprime la lista

--- ejercicio75.py
+++ ejercicio75.py
@@ -1,2 +1,10 @@
 '''Definir una lista vacía y luego solicitar la carga de 5 enteros por teclado y añadirlos a la lista. Imprimir la lista generada.'''
 #https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=15&codigo=15&inicio=0
+
+lista=[]
+x=0
+for x in range(5):
+    valor=int(input("Ingrese un numero entero: "))
+    lista.append(valor)
+    x=x+1
+print("los valores de la lista son:",lista)
