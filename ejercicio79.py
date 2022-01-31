a81478082938e5a90c972c7fcef3f22391d21d8b
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-31 10:25:40 -0400

    commit

---
+++ ejercicio79.py
@@ -0,0 +1,4 @@
+"""Una empresa tiene dos turnos (mañana y tarde) en los que trabajan 8 empleados (4 por la mañana y 4 por la tarde) 
+Confeccionar un programa que permita almacenar los sueldos de los empleados agrupados en dos listas.
+Imprimir las dos listas de sueldos."""
+
