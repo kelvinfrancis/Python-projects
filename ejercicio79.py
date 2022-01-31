55736d322c77cfdcb36c3e6ebbb9a3a63b49f7df
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-31 12:21:54 -0400

    ingresa 8 empleados, 4 en la lista de la mañana y 4 en la lista de la tarde, imprime las dos listas con los sueldos

--- ejercicio79.py
+++ ejercicio79.py
@@ -2,3 +2,14 @@
 Confeccionar un programa que permita almacenar los sueldos de los empleados agrupados en dos listas.
 Imprimir las dos listas de sueldos."""
 
+lista_dia=[]
+lista_tarde=[]
+sueldo_tarde=0
+while len(lista_dia)<4:
+    sueldo_dia=int(input("Ingrese los sueldos de los empleados de la mañana:"))
+    lista_dia.append(sueldo_dia)  
+while len(lista_tarde)<4:
+    sueldo_tarde=int(input("Ingrese los sueldos de los empleados de la tarde:"))
+    lista_tarde.append(sueldo_tarde)
+print("Salarios del turno de la mañana:",lista_dia)
+print("Salarios del turno de la tarde:",lista_tarde)
