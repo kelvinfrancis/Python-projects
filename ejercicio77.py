b97560cb30f3976e25314bce15f1c7dcebfa86d5
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-01-31 09:03:02 -0400

    Ingresa una lista de 5 sueldos, imprime la lista y el promedio de estos

--- ejercicio77.py
+++ ejercicio77.py
@@ -1,3 +1,12 @@
 "Almacenar en una lista los sueldos (valores float) de 5 operarios. Imprimir la lista y el promedio de sueldos"
 
+lista_sueldos=[]
+suma=0
+while len(lista_sueldos)<5:
+    sueldo=int(input("Ingrese un sueldo:"))
+    lista_sueldos.append(sueldo)
+    suma=suma+sueldo
+promedio=suma/len(lista_sueldos)
+print("Lista de sueldos:",lista_sueldos)
+print("Promedio:",promedio)
 
