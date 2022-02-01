a659c4a109fceb7203f53da92d0db6d28c742f65
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-01 14:43:22 -0400

    ingresa 5 nombres, te indica el menor alfabeticamente

--- ejercicio82.py
+++ ejercicio82.py
@@ -1,1 +1,8 @@
 "Ingresar por teclado los nombres de 5 personas y almacenarlos en una lista. Mostrar el nombre de persona menor en orden alfabético."
+
+lista=[]
+while len(lista)<5:
+    valor=input("Ingrese un nombre:")
+    lista.append(valor)
+print("Lista",lista)
+print("El nombre  menor en orden alfabetico es:", min(lista))
