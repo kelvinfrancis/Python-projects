7066d293352a89070dcb7a79d05c172dd3088b2d
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-02 12:26:25 -0400

    Ingresa 5 productos con sus precios, imprime los productos con mayor precio al primer ingresado

---
+++ ejercicio85.py
@@ -0,0 +1,15 @@
+'''Crear y cargar dos listas con los nombres de 5 productos en una y sus respectivos precios en otra. Definir dos listas paralelas. 
+Mostrar cuantos productos tienen un precio mayor al primer producto ingresado.'''
+
+productos=[]
+precios=[]
+while len(productos)<5:
+    prod=input("Ingrese el nombre del producto:")
+    productos.append(prod)
+    precio=float(input("Ingrese el precio del producto: RD$"))
+    precios.append(precio)
+x=0
+print("Los productos con precio mayor al primer producto ingresado""(",productos[0],") - RD$",precios[0])
+for x in range(5):
+    if precios[x]>precios[0]:
+        print("*",productos[x],"- RD$",precios[x])
