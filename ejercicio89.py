d7fdbbd3149da10e65184b859a69c330b61e5ace
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-08 09:44:59 -0400

    Codigo mas simplificado, ordena la lista de menor a mayor

--- ejercicio89.py
+++ ejercicio89.py
@@ -5,28 +5,10 @@ while len(sueldos)<5:
     valor=int(input("Ingrese un sueldo:"))
     sueldos.append(valor)
 print("Lista sin ordenar",sueldos)
-aux=0
-for x in range(4):
-    if sueldos[x]>sueldos[x+1]:
-        aux=sueldos[x]
-        sueldos[x]=sueldos[x+1]
-        sueldos[x+1]=aux
-x=1
-for x in range(3):
-    if sueldos[x]>sueldos[x+1]:
-        aux=sueldos[x]
-        sueldos[x]=sueldos[x+1]
-        sueldos[x+1]=aux
-x=2
-for x in range(2):
-    if sueldos[x]>sueldos[x+1]:
-        aux=sueldos[x]
-        sueldos[x]=sueldos[x+1]
-        sueldos[x+1]=aux
-x=3
-for x in range(1):
-    if sueldos[x]>sueldos[x+1]:
-        aux=sueldos[x]
-        sueldos[x]=sueldos[x+1]
-        sueldos[x+1]=aux
+for k in range(4):
+    for x in range(4):
+        if sueldos[x]>sueldos[x+1]:
+            aux=sueldos[x]
+            sueldos[x]=sueldos[x+1]
+            sueldos[x+1]=aux
 print("Lista ordenada de menor a mayor",sueldos)
