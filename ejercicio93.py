334b0e58645b608a0c072cacbde4253823c891cd
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-09 08:05:38 -0400

    commit

---
+++ ejercicio93.py
@@ -0,0 +1,5 @@
+"""Confeccionar un programa que permita cargar los nombres de 5 alumnos y sus notas respectivas. 
+Luego ordenar las notas de mayor a menor. Imprimir las notas y los nombres de los alumnos.
+Debe quedar claro que cuando intercambiamos las notas para dejarlas ordenadas de mayor a menor debemos 
+intercambiar los nombres para que las listas continúen paralelas 
+(es decir que en los mismos subíndices de cada lista continúe la información relacionada)"""
