27f7d77d9fd32dfa49bfcf1c631126f54b9ceadc
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-09 12:50:53 -0400

    Ingresa dos listas, una de nombres y otra de notas, imprime las listas ordenadas con las notas de mayor a menor.

--- ejercicio93.py
+++ ejercicio93.py
@@ -3,3 +3,26 @@ Luego ordenar las notas de mayor a menor. Imprimir las notas y los nombres de lo
 Debe quedar claro que cuando intercambiamos las notas para dejarlas ordenadas de mayor a menor debemos 
 intercambiar los nombres para que las listas continúen paralelas 
 (es decir que en los mismos subíndices de cada lista continúe la información relacionada)"""
+alumnos=[]
+notas=[]
+for x in range(5):
+    nombre=input("Ingrese el nombre del alumno: ")
+    alumnos.append(nombre)
+    nota=int(input("Ingrese la nota del alumno:"))
+    notas.append(nota)
+print("---Lista de alumnos y sus notas:")
+for x in range(5):
+    print(notas[x],alumnos[x])
+#orden de mayor a menor
+for k in range(4):
+    for x in range(4-k):
+        if notas[x]<notas[x+1]:
+            aux=notas[x]
+            aux1=alumnos[x]
+            notas[x]=notas[x+1]
+            alumnos[x]=alumnos[x+1]
+            notas[x+1]=aux
+            alumnos[x+1]=aux1
+print("---Lista ordenada de mayor a menor:")
+for x in range(5):
+    print(notas[x],alumnos[x])
