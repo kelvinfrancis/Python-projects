43708a5927860b214c6b9625b828a69fb5249263
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-10 08:26:29 -0400

    crear 4 listas de 3 elementos dentro de una lista. imprimir el resueltado

---
+++ ejercicio95.py
@@ -0,0 +1,8 @@
+"""Crear una lista por asignación. La lista tiene que tener cuatro elementos. 
+Cada elemento debe ser una lista de 3 enteros.
+Imprimir sus elementos accediendo de diferentes modos."""
+
+lista=[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+print(lista)
+for x in range(4):
+    print(x,"-",lista[x])
