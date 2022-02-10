9d873ef5556610960d4d8fdd1c00568477a6feb5
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-10 08:42:26 -0400

    commit

---
+++ ejercicio96.py
@@ -0,0 +1,2 @@
+"""Crear una lista por asignación. La lista tiene que tener 2 elementos. Cada elemento debe ser una lista de 5 enteros.
+Calcular y mostrar la suma de cada lista contenida en la lista principal."""
