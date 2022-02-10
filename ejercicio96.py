757c5359c2129d812ec22c736f3272437f6a8bb3
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-10 09:04:22 -0400

    Suma de dos listas de 5 elementos

--- ejercicio96.py
+++ ejercicio96.py
@@ -1,2 +1,12 @@
 """Crear una lista por asignación. La lista tiene que tener 2 elementos. Cada elemento debe ser una lista de 5 enteros.
 Calcular y mostrar la suma de cada lista contenida en la lista principal."""
+
+lista=[[1,2,3,4,5],[1,2,3,4,5]]
+for x in range(len(lista)):
+    suma=0
+    for k in range(len(lista[x])):
+        suma=suma+lista[x][k]
+print("La suma total es:",suma)
+
+
+    
