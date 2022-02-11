679bf2c7bca9312d176493ba141f006f05b0eec1
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-11 15:39:13 -0400

    commit

---
+++ ejercicio97.py
@@ -0,0 +1,5 @@
+"""Crear una lista por asignación. La lista tiene que tener 5 elementos.
+ Cada elemento debe ser una lista, la primera lista tiene que tener un elemento, la segunda dos elementos, 
+ la tercera tres elementos y así sucesivamente. Sumar todos los valores de las listas."""
+
+ 
