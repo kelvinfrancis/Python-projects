fde638f8d8543ae8df13b770f520ce62ecd8ce2e
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-14 15:11:56 -0400

    Modifica a 0 todos los valores mayores de 10 en la lista

---
+++ ejercicio99.py
@@ -0,0 +1,14 @@
+"""Se tiene la siguiente lista:
+lista=[[4,12,5,66], [14,6,25], [3,4,5,67,89,23,1], [78,56]]
+Imprimir la lista. Luego fijar con el valor cero todos los elementos mayores a 10 contenidos
+ en todos los elementos de la variable "lista". Volver a imprimir la lista."""
+
+lista=[[4,12,5,66], [14,6,25], [3,4,5,67,89,23,1], [78,56]]
+print("Lista original")
+print(lista)
+for x in range(len(lista)):
+    for k in range(len(lista[x])):
+        if lista[x][k]>10:
+            lista[x][k]=0
+print("Lista modificada:")
+print(lista)
