db284bbbc01902f5aa7b3ce7e41cc90f85d329c0
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-14 14:09:12 -0400

    commit

---
+++ ejercicio98.py
@@ -0,0 +1,5 @@
+"""Se tiene la siguiente lista:
+lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
+Imprimir la lista. Luego fijar con el valor cero todos los elementos mayores a 50 del primer elemento de "lista".
+Volver a imprimir la lista."""
+
