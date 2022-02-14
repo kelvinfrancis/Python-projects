d135185ea97b8ffebccf7277f00b36b05d91fda9
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-14 14:32:36 -0400

    Convierte en 0 los numeros mayores a 50 del primer elemento de la lista

--- ejercicio98.py
+++ ejercicio98.py
@@ -3,3 +3,10 @@ lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
 Imprimir la lista. Luego fijar con el valor cero todos los elementos mayores a 50 del primer elemento de "lista".
 Volver a imprimir la lista."""
 
+lista=[[100,7,85,8], [4,8,56,25], [67,89,23,1], [78,56]]
+print(lista)
+for x in range(len(lista)):
+    if lista[0][x]>50:
+        lista[0][x]=0
+print("Lista remodelada:")
+print(lista)
