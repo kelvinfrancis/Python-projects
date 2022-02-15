480a5bc00e8338669e1a6b45efe332003126ac2a
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-15 10:13:23 -0400

    commit

---
+++ ejercicio101.py
@@ -0,0 +1,3 @@
+"""Crear y cargar una lista con los nombres de tres alumnos. 
+Cada alumno tiene dos notas, almacenar las notas en una lista paralela. 
+Cada componente de la lista paralela debe ser también una lista con las dos notas. Imprimir luego cada nombre y sus dos notas."""
