40c78f4493296808461a10a6cd4c4b7d233152b4
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-15 15:19:03 -0400

    commit

---
+++ ejercicio102.py
@@ -0,0 +1,9 @@
+"""Se tiene que cargar la siguiente información:
+· Nombres de 3 empleados
+· Ingresos en concepto de sueldo, cobrado por cada empleado, en los últimos 3 meses.
+Confeccionar el programa para:
+
+a) Realizar la carga de los nombres de empleados y los tres sueldos por cada empleado.
+b) Generar una lista que contenga el ingreso acumulado en sueldos en los últimos 3 meses para cada empleado.
+c) Mostrar por pantalla el total pagado en sueldos a cada empleado en los últimos 3 meses
+d) Obtener el nombre del empleado que tuvo el mayor ingreso acumulado"""
