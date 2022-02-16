bd0e90bd94bb5abff0a52854595a2c43aa5358aa
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-16 07:59:31 -0400

    commit

---
+++ ejercicio103.py
@@ -0,0 +1,3 @@
+"""Solicitar por teclado dos enteros. El primer valor indica la cantidad de elementos que crearemos en la lista. El segundo valor indica la cantidad de elementos que tendrá cada una de las listas internas a la lista principal.
+Mostrar la lista y la suma de todos sus elementos."""
+
