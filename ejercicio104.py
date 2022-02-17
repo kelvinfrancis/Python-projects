43767c8650bdf11a268b0dd0806afa8e24df5e47
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-17 08:32:09 -0400

    commi

---
+++ ejercicio104.py
@@ -0,0 +1,6 @@
+"""Definir dos listas de 3 elementos.
+La primer lista cada elemento es una sublista con el nombre del padre y la madre de una familia.
+La segunda lista está constituida por listas con los nombres de los hijos de cada familia. Puede haber familias sin hijos.
+Imprimir los nombres del padre, la madre y sus hijos.
+También imprimir solo el nombre del padre y la cantidad de hijos que tiene dicho padre."""
+
