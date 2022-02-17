53e88b4dbd16f501e772b35f1c47a9c5c8f16c46
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-02-17 16:54:15 -0400

    commi

--- ejercicio104.py
+++ ejercicio104.py
@@ -4,3 +4,20 @@ La segunda lista está constituida por listas con los nombres de los hijos de ca
 Imprimir los nombres del padre, la madre y sus hijos.
 También imprimir solo el nombre del padre y la cantidad de hijos que tiene dicho padre."""
 
+padres=[]
+hijos=[]
+for x in range(3):
+    padres.append([])
+    hijos.append([])
+    for k in range(3):
+        papa=input("Ingrese el nombre del padre: ")
+        mama=input("Ingrese el nombre de la madre: ")
+        sons=input("Ingrese los nombres de los hijos. Si no tienen, escribir -ninguno-: ")
+        padres[x].append(papa,mama)
+        hijos[x].append(sons)
+for x in range(len(padres)):
+    print("Lista de padres e hijos:")
+    print(padres[x],hijos[x])
+
+
+
