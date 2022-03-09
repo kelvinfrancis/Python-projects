e8f2bf4953ca7e07cb2a095f5b7a59bab431ff22
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-03-09 09:37:24 -0400

    commit

---
+++ ejercicio105.py
@@ -0,0 +1,8 @@
+"""Se desea saber la temperatura media trimestral de cuatro paises. Para ello se tiene como dato las temperaturas medias mensuales de dichos paises.
+Se debe ingresar el nombre del país y seguidamente las tres temperaturas medias mensuales.
+Seleccionar las estructuras de datos adecuadas para el almacenamiento de los datos en memoria.
+a - Cargar por teclado los nombres de los paises y las temperaturas medias mensuales.
+b - Imprimir los nombres de las paises y las temperaturas medias mensuales de las mismas.
+c - Calcular la temperatura media trimestral de cada país.
+c - Imprimir los nombres de los paises y las temperaturas medias trimestrales.
+b - Imprimir el nombre del pais con la temperatura media trimestral mayor."""
