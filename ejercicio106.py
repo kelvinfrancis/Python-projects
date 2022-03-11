1f10a74c5e3cc9e1ae19c0f099c18c0daf25d8e2
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-03-11 12:32:33 -0400

    commit

---
+++ ejercicio106.py
@@ -0,0 +1,6 @@
+#https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=21&codigo=21&inicio=15
+"""Definir una lista y almacenar los nombres de 3 empleados.
+Por otro lado definir otra lista y almacenar en cada elemento una sublista con los números de días del mes que el empleado faltó.
+Imprimir los nombres de empleados y los días que faltó.
+Mostrar los empleados con la cantidad de inasistencias.
+Finalmente mostrar el nombre o los nombres de empleados que faltaron menos días."""
