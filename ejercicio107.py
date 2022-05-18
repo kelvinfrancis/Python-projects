7fbc6e55c0782c5ab53d40016c672fd21edeeaa0
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-05-18 16:37:53 -0400

    commit

---
+++ ejercicio107.py
@@ -0,0 +1,3 @@
+"""Desarrollar un programa que cree una lista de 50 elementos. El primer elemento es una lista con un elemento entero, el segundo elemento es una lista de dos elementos etc.
+La lista debería tener esta estructura y asignarle esos valores a medida que se crean los elementos:
+[[1], [1,2], [1,2,3], [1,2,3,4], [1,2,3,4,5], etc....]"""
