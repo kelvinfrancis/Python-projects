31264afa90e23503dcb4e25931f7ff46867ab189
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-05-19 15:48:23 -0400

    commit

---
+++ ejercicio108.py
@@ -0,0 +1,11 @@
+"""Crear una lista por asignación con 5 enteros. 
+Eliminar el primero, el tercero y el último de la lista."""
+
+lista=[1,2,3,4,5]
+print(lista)
+lista.pop(0)
+print(lista)
+lista.pop(1)
+print(lista)
+lista.pop(2)
+print(lista)
