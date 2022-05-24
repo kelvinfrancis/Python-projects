7e78271e6f74f48c6741231905600a02c87875d5
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2022-05-24 09:52:21 -0400

    ejercicio110

---
+++ ejercicio110.py
@@ -0,0 +1,3 @@
+"""Crear dos listas paralelas. En la primera ingresar los nombres de empleados y en la segunda los sueldos de cada empleado.
+Ingresar por teclado cuando inicia el programa la cantidad de empleados de la empresa.
+Borrar luego todos los empleados que tienen un sueldo mayor a 10000 (tanto el sueldo como su nombre)"""
